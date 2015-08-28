531bed00d052f0917851a61ef9100d5a32ec733d
liufenghua <liufenghua@vivo.com.cn>
2015-08-28 18:11:19 +0800

    mybranch第一次修改提交

--- src/main/java/com/example/myappinfo/MainActivity.java
+++ src/main/java/com/example/myappinfo/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends Activity {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-               String info=(boolean)(ApplistItem.get(position)).get("isSys")?"是非系统的应用":"是系统的应用";
+               String info=(boolean)(ApplistItem.get(position)).get("isSys")?"是非系统的应用啊":"是系统的应用啊";
                Toast.makeText(MainActivity.this, ApplistItem.get(position).get("name")+info+"heihei", Toast.LENGTH_LONG).show();
             }
             
